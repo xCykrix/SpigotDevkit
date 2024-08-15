3c42960fa8be54b27294232e2089fc5fafe1df4e
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-08-15 16:19:28 -0400

    feat: update source

--- src/main/java/github/xCykrix/plugin/H2MVStorePlugin.java
+++ src/main/java/github/xCykrix/plugin/H2MVStorePlugin.java
@@ -3,6 +3,7 @@ package github.xCykrix.plugin;
 import github.xCykrix.DevkitPlugin;
 import github.xCykrix.extendable.DevkitFullState;
 import java.io.File;
+import java.util.Date;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.h2.mvstore.MVStore;
 import org.h2.mvstore.MVStoreException;
@@ -27,13 +28,25 @@ public class H2MVStorePlugin extends DevkitFullState {
           .fileName(this.plugin.getDataFolder() + "/" + "h2store")
           .open();
     } catch (MVStoreException exception) {
+      // Move Database to Migration Backup
       File file = new File(this.plugin.getDataFolder() + "/" + "h2store");
+      String name = "h2store.restore." + (new Date()).getTime();
       if (file.exists()) {
-        file.renameTo(new File(this.plugin.getDataFolder() + "/" + "h2store.legacy"));
+        file.renameTo(new File(this.plugin.getDataFolder() + "/" + name));
       }
-      this.plugin.getLogger().warning("Failed to load h2store. Database corrupted or unsupported version.");
-      this.plugin.getLogger().warning("Please report this to Plugin Author. Database has been saved as 'h2store.legacy' and regenerated.");
-      this.plugin.getLogger().warning(ExceptionUtils.getMessage(exception));
+
+      // Attempt to Migrate
+      if (ExceptionUtils.getMessage(exception).contains("write format")) {
+        this.plugin.getLogger().warning("Attempting to run migrations on h2store from v1 to v3 database format. This is a one time operation.");
+
+        return;
+      }
+
+
+      this.plugin.getLogger().warning("Failed to load h2store. Database corrupted or failed to migrate.");
+      this.plugin.getLogger().warning("Please report this to Plugin Author. Database has been saved as 'h2store.legacy' and initialized.");
+      this.plugin.getLogger().warning("This message indicates that your h2store flat file database is damaged and may require manual repairs.");
+      this.plugin.getLogger().warning("Error Message: " + ExceptionUtils.getMessage(exception));
       this.store = null;
     }
   }
