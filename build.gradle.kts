47cfe43422707fcdedb664131355ded89fc278db
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-04-29 13:41:55 +0000

    feat: update to 1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,15 +29,15 @@ repositories {
 // Dependencies
 dependencies {
     // Spigot-based API
-    compileOnly("org.spigotmc:spigot-api:1.21.3-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.5-R0.1-SNAPSHOT")
 
     // Bundled Libraries
     implementation("org.apache.commons:commons-lang3:3.17.0")
-    implementation("net.kyori:adventure-api:4.17.0")
+    implementation("net.kyori:adventure-api:4.20.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.4")
-    implementation("net.kyori:adventure-text-minimessage:4.17.0")
+    implementation("net.kyori:adventure-text-minimessage:4.20.0")
     implementation("dev.dejvokep:boosted-yaml-spigot:1.4")
-    implementation("dev.jorel:commandapi-bukkit-shade:9.7.0")
+    implementation("dev.jorel:commandapi-bukkit-shade:10.0.0")
     implementation("com.h2database:h2-mvstore:2.3.232")
 
     // APIs
